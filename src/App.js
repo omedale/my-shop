9aa45d623c911017c693eeb2f02ecb6d3ad466e4
omedale <omedale@gmail.com>
2019-07-26 11:47:11 +0100

    intial commit :fire:

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App">
+        <h1>Test</h1>
+        <Button type="primary">Button</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
